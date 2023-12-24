4ebaca699f0cb210b8d27f494e7d5100b1d93f3d
Lantern Elf <114974407+Lantern-elf@users.noreply.github.com>
2023-12-24 20:08:16 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+
+function info_alka(){
+    document.getElementById('info').innerHTML = '<b style="font-size: 25px;">Alka</b><p>He used to be a human from the outskirt of Anima City. But after his first encounter with Lucafoxx, he requested to be a testing subject as well.</p><p>And now he lived his life as a wolf, alongside his newfound brother in arms. Traveling through multiple dimensions and the universe side by side together.</p>'
+    document.getElementById('command').innerHTML = '<div></div>'
+}
+
+function info_luca(){
+    document.getElementById('info').innerHTML = '<b style="font-size: 25px;">Lucafoxx</b><p>A psychic fox with several types of skills and abilities.</p><p>He was a human that has been saved from a near-death experience through a special surgery, turning himself into a half-pokemon and half-fox as a result.</p><p>He works as a professor assistant by collecting data remotely and now lives together with his wolf brother.</p>'
+    document.getElementById('command').innerHTML = '<div></div>'
+}
+
+function info_skyler(){
+    document.getElementById('info').innerHTML = '<b style="font-size: 25px;">Skyler The Hadgehog</b><p>An Indonesian virtual tech mobian hedgehog Youtuber.</P><p>Works in G.U.N alongside his wife. Loves to share his experience in programming and games.</p><p>Now he lives with his wife and his 2 kids.</P>'
+    document.getElementById('command').innerHTML = '<div></div>'
+}
+
+parent='max-w-full'
